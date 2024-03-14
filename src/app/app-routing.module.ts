5ad3eed5a0050cd506da3a4e0ae2daa731073ec7
SebastianKAimars <sebastiank@aimars.io>
2024-03-14 18:54:16 +0100

    install Angular material

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { AppComponent } from "./app.component";
+
+const routes: Routes = [
+   
+  ];
+  
+  @NgModule({
+    imports: [RouterModule.forRoot(routes, {
+      scrollPositionRestoration: 'enabled',
+      useHash: true,
+    })],
+    exports: [RouterModule],
+  })
+  export class AppRoutingModule {}
